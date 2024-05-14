9cb6d085ef12bc6f80da2553dfe6e729c2dc7281
bailiang9966 <bailiang9966@gmail.com>
2024-05-14 19:42:51 +0800

    33

--- main.py
+++ main.py
@@ -104,7 +104,7 @@ class HAHA():
         try:
             
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            sock.settimeout(0.1)
+            sock.settimeout(0.5)
             result = sock.connect_ex((host, port))
             if result == 0:
                 test_result= True
